8bce95cdd3c30d362e77b785a4eea429b8a50506
Zeyitcan DAŞDEMİR <zytcn01@outlook.com>
2025-12-27 20:39:37 +0300

    Implemented file service for photo saving.

---
+++ Persistence/Data/File/FileService.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces.File;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Persistence.Data.File
+{
+    public class FileService(
+        IWebHostEnvironment env
+        ) : IFileService
+    {
+        public async Task<List<string>> SavePhotosAsync(IFormFileCollection files, string folderName)
+        {
+
+            var filePaths = new List<string>();
+            string rootPath = env.WebRootPath ?? Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            string uploadPath = Path.Combine(rootPath, "Photos", folderName);
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    // Getting file extension
+                    string extension = Path.GetExtension(file.FileName);
+
+                    // Creating unique file name
+                    string newFileName = $"{Guid.NewGuid()}{extension}";
+
+                    // Path to save
+                    string fullPath = Path.Combine(uploadPath, newFileName);
+
+                    // Saving the file
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+
+                    var dbPath = Path.Combine("Photos", folderName, newFileName).Replace("\\", "/");
+                    filePaths.Add(dbPath);
+                }
+            }
+            return filePaths;
+        }
+    }
+}
